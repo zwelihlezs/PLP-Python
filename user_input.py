00a8ce1f084666b6301c46acc985fdcf860bd3a7
Zwelihle Nhlapho <107548375+zwelihlezs@users.noreply.github.com>
2023-12-01 08:58:37 +0200

    Create user_input.py
    take user input

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("What's your name: ")
+age = input(name + " How old are you: ")
+location = input("Where do you stay: ")
+
+
+print("Great to meet you " + name + ", you're "+ age + " years old.\n " + location + " is a beatiful place. ")
