c84987965b23ab6769f0eab6dca2b8a8cc4b5fbd
Zwelihle Nhlapho <107548375+zwelihlezs@users.noreply.github.com>
2023-12-01 09:52:58 +0200

    Add files via upload
    my_list

---
+++ my_list.py
@@ -0,0 +1,18 @@
+my_list = []
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+my_list.insert(1, 15)
+
+my_list.extend([50,60,70])
+
+my_list.pop()
+
+my_list.sort()
+
+a_index = my_list.index(30)
+
+print(a_index)
